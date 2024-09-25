5acda35b390babc8cb44b2b9eaeac234fbbf6556
Thailan Gouvea <thailangm@icloud.com>
2024-09-25 09:22:04 +0300

    Implements collapsable menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import "App.css";
+import { useState } from "react";
+import "./App.css";
 import { CgProfile } from "react-icons/cg";
 
 export default function App() {
+  const [menuToggle, setMenuToggle] = useState(false);
   return (
     <div className="App">
       <div className="header">
-        <span className="menuToggle">=</span>
+        <span
+          className="menuToggle"
+          onClick={() => {
+            setMenuToggle(!menuToggle);
+          }}
+        >
+          =
+        </span>
         <h1>Logo</h1>
         <CgProfile className="profilePic" />
       </div>
+      <nav style={{ display: menuToggle ? "block" : "none" }}>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      </nav>
     </div>
   );
 }

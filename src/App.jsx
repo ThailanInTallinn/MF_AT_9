1493bcb135f0f4389663b6cad3bb7a4e521a99d1
Thailan Gouvea <thailangm@icloud.com>
2024-09-25 09:07:23 +0300

    Adds basic elements to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import "App.css";
+import { CgProfile } from "react-icons/cg";
 
 export default function App() {
-  return <div className="App">{}</div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <span className="menuToggle">=</span>
+        <h1>Logo</h1>
+        <CgProfile className="profilePic" />
+      </div>
+    </div>
+  );
 }
